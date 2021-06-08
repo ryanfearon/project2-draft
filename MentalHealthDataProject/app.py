575c9649bf866a89f72c0f74bc6cf84340a16bd7
ryanfearon <77812707+ryanfearon@users.noreply.github.com>
2021-06-08 18:25:36 -0400

    Update app.py

--- MentalHealthDataProject/app.py
+++ MentalHealthDataProject/app.py
@@ -1,5 +1,14 @@
 # import necessary libraries
-from flask import Flask, render_template
+
+import pandas as pd
+import numpy as np
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -29,6 +38,43 @@ POSTGRES = {
 }
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Mental_health = Base.classes.mental_health
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+@app.route("/physical")
+def physical():
+    """Return a list of sample names."""
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])
+
+@app.route("/metadata/<sample>")
+def sample_metadata(sample):
+    """Return the MetaData for a given sample."""
+    sel = [
+        Mental_health.year,
+        Mental_health.physical_importance,
+        Mental_health.mental_importance,
+        Mental_health.industry_support,
+
+    ]
+
+    results = db.session.query(*sel).all()
+
+    print(sample_metadata)
+    return jsonify(sample_metadata)
 
 if __name__ == "__main__":
     app.run(debug=True)
