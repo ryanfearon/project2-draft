bc10b3288b99371fa0545092460938853b495cee
ryanfearon <77812707+ryanfearon@users.noreply.github.com>
2021-06-07 21:36:13 -0400

    Update app.py

--- MentalHealthDataProject/app.py
+++ MentalHealthDataProject/app.py
@@ -1,1 +1,34 @@
-# import necessary librariesfrom flask import Flask, render_template# create instance of Flask appapp = Flask(__name__)# create route that renders index.html template@app.route("/")def index():    return render_template("index.html")if __name__ == "__main__":    app.run(debug=True)    # Using Postgres to load dataPOSTGRES = {    'user': 'postgres',    'pw': 'postgres',    'db': 'mentalhealth_db',    'port': '5433',}app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/mentalhealth_db'POSTGRES
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+    
+# Using Postgres to load data
+# POSTGRES = {
+#     'user': 'postgres',
+#     'pw': 'postgres',
+#     'db': 'mentalhealth_db',
+#     'port': '5432',
+# }
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:delilahjones@localhost:5432/mentalhealth_db'
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'delilahjones',
+    'db': 'mentalhealth_db',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+if __name__ == "__main__":
+    app.run(debug=True)
